1e63df4d883af9b5ab72797545ff494e063b138d
MarijanaHornjak <marijanahornjak95@gmail.com>
2023-03-15 11:30:10 +0100

    finished

--- src/components/SalonServices/SalonServices.js
+++ src/components/SalonServices/SalonServices.js
@@ -3,8 +3,6 @@ import "./SalonServices.scss";
 import SalonGirl from "../../assets/images/1788.png";
 import SalonHair from "../../assets/images/pexels-engin-akyurt-3065209.png";
 import SalonIcon from "./SalonIcon";
-import Flower2 from "../../assets/flowers&leafs//cvet 2.png";
-import Leaf3 from "../../assets/flowers&leafs/3.png";
 
 const SalonServices = () => {
   return (
